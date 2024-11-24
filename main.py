745fe8890972d67e6a404b6eeaecca90c62303c0
Tarun Gudapati <g.tarun25@gmail.com>
2024-11-24 11:45:17 +0530

    assignment5 submission

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 def min_coins(coins, target_amount):
-    ""code here""
+    dp_array = [target_amount+1 for i in range(target_amount+1)]
+    dp_array[0] = 0
+
+    for amount in range(1,target_amount+1):
+        for coin in coins:
+            if amount - coin >= 0:
+                dp_array[amount] = min(dp_array[amount], 1+ dp_array[amount -coin])
+    
+    return dp_array[target_amount] if dp_array[target_amount] != target_amount + 1 else -1
